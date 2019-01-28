f5c1bf6da274148539a79576b8370729ba8cecf0
R Steiner <r.steiner@uconn.edu>
2019-01-28 13:22:10 -0500

    Finish converting to csv from pandas.

--- freq_pos.py
+++ freq_pos.py
@@ -4,60 +4,32 @@
 Separate out the part of speech and sum the frequencies across years.
 """
 
-# import pandas as pd
 import csv
 
 
-def load_corpus(path):
-    headers = ["ngram", "year", "count", "volume_count"]
-    return pd.read_csv(path, sep="\t", low_memory=False, header=None,
-                       names=headers)
+def write_csv(output_path, freqs):
+    with open(output_path, "w", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=["word", "pos", "count"])
+        for row in freqs.values():
+            writer.writerow(row)
 
 
-def truncate_years(df, cutoff):
-    return df.where(df.year >= cutoff)
-
-
-def sum_frequencies(df):
-    grouped = df.groupby(["ngram"]).sum()
-    return grouped.reset_index()
-
-
-def split_pos(df):
-    df.pos = df.ngram.apply(lambda word: word.split("_")[-1])
-    df.ngram = df.ngram.apply(lambda word: word.split("_")[0])
-    return df
-
-
-def select_relevant(df):
-    return df.loc[:, ["ngram", "pos", "count"]]
-
-
-def main(file_path, cutoff):
-    # file_path = "./googlebooks-eng-all-1gram-20120701-a"
+def main(input_path, output_path, cutoff):
     freqs = {}
-    with open(file_path, "r", newline="") as f:
-        reader = csv.DictReader(f, fieldnames=["ngram", "year", "count",
-                                               "volume_count"])
+    headers = ["ngram", "year", "count", "volume_count"]
+    with open(input_path, "r", newline="") as f:
+        reader = csv.DictReader(f, fieldnames=headers)
         for row in reader:
-            # I want to do this as a separate function, but I think
-            # passing a dict back and forth is worse than updating an
-            # existing one on the fly.
             if int(row["year"]) >= cutoff:
-                if freqs.get(row["ngram"]):
-                    freqs[row["ngram"]] += int(row["count"])
+                if row["ngram"] in freqs:
+                    freqs[row["ngram"]]["freq"] += int(row["count"])
                 else:
-                    freqs[row["ngram"]] = int(row["count"])
-    df = load_corpus(file_path)
-    print("Corpus loaded.")
-    df = truncate_years(df, 1999)
-    print("Truncated years.")
-    df = sum_frequencies(df)
-    print("Summed frequencies.")
-    df = split_pos(df)
-    print("Split POS.")
-    df = select_relevant(df)
-    print("Selected relevant columns.")
-    df.to_csv("aggregated/a_1.csv", index=False)
+                    freqs[row["ngram"]] = {
+                        "freq": int(row["count"]),
+                        "word": row["ngram"].split("_")[0],
+                        "pos": row["ngram"].split("_")[-1]
+                    }
+    print("Calculated frequencies.")
+    write_csv(output_path, freqs)
     print("Saved to file.")
-    return df
+    return freqs
